bbe7938e73627d8af7f4c490383868df8a0fd115
vniej002 <juanr@bbd.co.za>
2023-05-21 11:04:18 +0200

    Required Query update

--- data-access/queries/userQueries.js
+++ data-access/queries/userQueries.js
@@ -4,4 +4,4 @@ exports.registerUser = `INSERT INTO appuser (username, email, passcode, city, pr
 exports.updateUser = `UPDATE appuser SET username = ?, email = ?, passcode = ?, city = ?, province = ? WHERE userId = ?`;
 exports.deleteUser = `DELETE FROM appuser WHERE userId = ?`;
 
-exports.validateUser = `SELECT username, email, passcode, city, province FROM appuser WHERE username = ? AND passcode = ?`;
+exports.validateUser = `SELECT userId, username, email, passcode, city, province FROM appuser WHERE username = ? AND passcode = ?`;
